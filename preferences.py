9842719ec2ea86ce28a6f9bc5535749bac06ebbc
punksolid <punksolid@e17250d6-9b91-8bb1-3a2a-51e2cffa4edf>
2010-09-25 00:44:02 +0000

    Made a menu and a half way to save it in a file.

--- preferences.py
+++ preferences.py
@@ -1,19 +1,24 @@
+import shelve
 class preferences:
     """This module will save the user preferences into a database"""
     def __init__(self):
+        opciones=[u'No',u'Yes']
+        self.preferencias=[(u'SMS\'s','combo',(opciones,0)),(u'Calls','combo',(opciones,0)),(u'Battery','combo',(opciones,0)),(u'Other','combo',(opciones,0))] #0 No, 1 Yes
         try:
             import cPickle as pickle
         except ImportError:
             import pickle
 
-    def save(self,numero):
+    def save(self,metalist):
         """Saves a number"""
         try:
             import cPickle as pickle
         except ImportError:
             import pickle
-        archivo = file("notifierpref.dat","w",2)
-        pickle.dump(numero,archivo)
+        archivo = file("notfierprf.dat","w",2)
+
+        for element in metalist:
+            pickle.dump(element,archivo)
         archivo.close()
 
     def leerNumero(self):
@@ -22,8 +27,13 @@ class preferences:
             import cPickle as pickle
         except ImportError:
             import pickle
-        archivo = file("notifierpref.dat")
-        numero=pickle.load(archivo)
-        return numero
+        try:
+            archivo = file("notfierprf.dat")
+            for i in range(3):
+                preferencias=pickle.load(archivo)
+        except EnvironmentError:
+            print "No existe archivo"
+            preferencias = self.preferencias
+        return preferencias
 
 
