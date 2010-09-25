84a07b5a8eb950776b0e17e5d9ccba8dfc2f8b89
punksolid <punksolid@e17250d6-9b91-8bb1-3a2a-51e2cffa4edf>
2010-09-25 00:44:02 +0000

    Made a menu and a half way to save it in a file.

--- trunk/messaging.py
+++ trunk/messaging.py
@@ -1,4 +1,4 @@
-import inbox
+import inbox, messaging
 class messaging:
     """Messaging Interface"""
     def __init__(self):
@@ -8,3 +8,12 @@ class messaging:
         """Retrieves a tuple of the last message content and contact, in that order"""
         id=i.sms_messages()[0]
         return i.content(id),i.address(id)
+
+    def send(self,number,message,fromautor=null):
+        def cb(state):
+            if state==messaging.ESent:
+                print "**Message was sent**"
+            if state==messaging.ESendFailed:
+                print "**Something went wrong - Truly sorry for this**"
+        messaging.sms_send(number, message, '7bit',cb, fromautor)
+
--- trunk/notifier.py
+++ trunk/notifier.py
@@ -1,11 +1,12 @@
 #from conection import *
+#from preferences import *
 #import appuifw,e32
 ##Just for debbuging needs and execfile because pys60 doesn't see files in the same directory
 ##with import until make the .sis package
 #execfile("E:\\data\\python\\messaging.py")
-#execfile("E:\\data\\python\\preferences.py")
+execfile("E:\\data\\python\\preferences.py")
 #execfile("E:\\data\\python\\conection.py")
-execfile("E:\\data\\python\\call.py") #call manager
+#execfile("E:\\data\\python\\call.py") #call manager
 #import messaging
 #sms=messaging()
 #content,contact=sms.last()
@@ -14,22 +15,7 @@ execfile("E:\\data\\python\\call.py") #call manager
 
 #Define the exit function
 
-#The title must be in unicode
-#appuifw.app.title=u"Symbian Notifier Preferences"
-#preferencias=(u'SMS\'s',u'Calls',u'Others')
-#opciones=[u'Yes',u'No']
-#preferencias=[(u'SMS\'s','combo',(opciones,0)),(u'Calls','combo',(opciones,1)),(u'Battery','combo',(opciones,1)),(u'Other','combo',(opciones,0))]
-#flags=appuifw.FFormEditModeOnly
-#f=appuifw.Form(preferencias,flags)
-#f.execute()
-#for campo in f:
-#    print campo
 
-#preferencias = appuifw.multi_selection_list([u'SMS\'s',u'Calls',u'Others'])
-#appuifw.multi_selection_list([u'SMS\'s',u'Calls',u'Others'])
-#preferencias=preferences()
-#preferencias.save(8)
-#print preferencias.leerNumero()
 
 #---SOCKETS TEST DESKTOP--------------------------------------------
 #mensaje=conection("v2/10/1/PING/6672100343/Homer")
@@ -39,11 +25,46 @@ execfile("E:\\data\\python\\call.py") #call manager
 #--------detecting incoming calls--------------------
 #Copyright (c) 2008 Pankaj Nathani
 #Waiting for a call
-call=call()
-print call.cb_calling
-telephone.call_state(cb_calling)
-#telephone.incoming_call()
-
-app_lock.wait()
+#call=call()
+#print call.cb_calling
+#telephone.call_state(cb_calling)
+##telephone.incoming_call()
+#
+#app_lock.wait()
 
 #----------------------------------------------------
+#----------------Detecting a message entry-----------------------------
+#import inbox
+#id=0
+#def cb(id_cb):
+#    global id
+#    id=id_cb
+#
+#i=inbox.Inbox()
+#i.bind(cb)
+## Send an SMS to your inbox here. The "id" gets updated
+#i.address(id)
+#i.content(id)
+
+#---------------------------------------------------------------
+#---------------------------User Interface----------------------
+import appuifw
+appuifw.app.title=u"Symbian Notifier Preferences"
+home = appuifw.selection_list([u'Preferencias',u'Enviar PING'])
+if(home==0):
+    """Formulario de preferencias"""
+    preferencias=preferences()
+    formulario=preferencias.leerNumero()
+    flags=appuifw.FFormEditModeOnly
+    f=appuifw.Form(formulario,flags)
+    f.execute()
+    preferencias.save(f)
+else:
+    print "PING"
+
+#preferencias = appuifw.multi_selection_list([u'SMS\'s',u'Calls',u'Others'])
+#appuifw.multi_selection_list([u'SMS\'s',u'Calls',u'Others'])
+#preferencias=preferences()
+#preferencias.save(8)
+#print preferencias.leerNumero()
+#--------------------------------------------------------
